9d58f903184992af35b06c46a655d9c065dbaf39
ersu-amine <ersu-amine@gmail.com>
2025-01-07 01:09:45 -0500

    chore(util): Add CukesRunner

--- src/test/java/com/appium/runners/CukesRunner.java
+++ src/test/java/com/appium/runners/CukesRunner.java
@@ -1,4 +1,21 @@
 package com.appium.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "pretty"
+        },
+        glue = "com/appium/step_def",
+        dryRun = true,
+        features = "src/test/resources/features",
+        tags = "@wip"
+)
+
 public class CukesRunner {
+
 }
